c8b86420d8b1c6280d6a13b2a6f7b51e69e12e4a
Slim-Jesus <indracosimo@gmail.com>
2025-11-20 18:18:41 +0100

    init

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <glm/glm.hpp>
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include <entt/entt.hpp>
+#include "imgui.h"
+#include <imgui_impl_glfw.h>
+#include <GLFW/glfw3.h> 
+
+int main() 
+{
+    return 0;
+}
